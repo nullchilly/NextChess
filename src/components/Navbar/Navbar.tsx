d3621399804363febd501f88685d9933ff68bcd8
sownfam <ptson.0403@gmail.com>
2023-10-31 11:32:44 +0700

    feat: home

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,7 @@
 "use client";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 import React from "react";
-import * as NavigationMenu from "@radix-ui/react-navigation-menu";
 
-import { CaretRightIcon } from "@radix-ui/react-icons";
 import StockChess from "@/components/icons/StockChess";
 import ChessBoard from "@/components/icons/ChessBoard";
 import Puzzle from "@/components/icons/Puzzle";
@@ -12,117 +10,66 @@ import Human from "@/components/icons/Human";
 import Duel from "@/components/icons/Duel";
 import Video from "@/components/icons/Video";
 import Learn from "@/components/icons/Learn";
+import { ConfigProvider, Menu } from "antd";
+import type { MenuProps } from "antd";
+import Link from "next/link";
 
-const ROUTES = [
-  { href: "/random", text: "Random" },
-  { href: "/computer", text: "Computer" },
-  { href: "/minimax", text: "Minimax" },
+type MenuItem = Required<MenuProps>["items"][number];
+
+function getItem(
+  label: React.ReactNode,
+  key: React.Key,
+  icon?: React.ReactNode,
+  children?: MenuItem[],
+  type?: "group"
+): MenuItem {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  } as MenuItem;
+}
+
+const items: MenuProps["items"] = [
+  getItem("Play", "sub1", <ChessBoard />, [
+    getItem("Play vs Human", "/login", <Human />),
+    getItem("Play vs Bot", "/computer", <ChessBot />),
+  ]),
+
+  // TODO: Change path to the corresponding component
+  getItem("Puzzle", "sub2", <Puzzle />, [
+    getItem("Play Puzzle", "/puzzle", <Puzzle />),
+    getItem("Puzzle Duel", "/puzzle-duel", <Duel />),
+  ]),
+
+  getItem("Learn", "sub3", <Learn />, [
+    getItem("Videos", "/videos", <Video />),
+  ]),
 ];
 
 const Navbar = () => {
+  const router = useRouter();
+  const onClick: MenuProps["onClick"] = (e) => {
+    let href = e.keyPath[0]; // WARNING: This's hacky...
+    router.push(href);
+  };
   return (
-    <div className="bg-[#477330] px-4 py-4 grow text-center" style={{maxWidth: '272px'}}>
-      <div>
+    <div
+      className="bg-[#477330] px-4 py-4 grow text-center"
+      style={{ maxWidth: "272px", minWidth: "268px" }}
+    >
+      <Link href="/">
         <StockChess />
-      </div>
-      <div className="text-white font-['Albula'] text-xl">
-        <NavigationMenu.Root orientation="vertical">
-          <NavigationMenu.List className="flex flex-col gap-4">
-            <NavigationMenu.Item >
-              <NavigationMenu.Trigger asChild className="cursor-pointer hover:bg-[#538538]">
-                <div className="flex flex-row items-center justify-between px-4">
-                  <div className="flex flex-row gap-4">
-                    <ChessBoard />
-                    <span> Play </span>
-                  </div>
-                  <CaretRightIcon
-                    width={24}
-                    height={24}
-                    color="white"
-                    fontWeight={"bold"}
-                  />
-                </div>
-              </NavigationMenu.Trigger>
-              <NavigationMenu.Content>
-                <div className="one m-0 grid list-none gap-x-[8px] p-[18px] sm:w-[500px] sm:grid-cols-[0.75fr_1fr] gap-4">
-                  <Link className="col-span-2 grid w-fit hover:bg-[#538538]" href='/login'>
-                    <div className="flex flex-row gap-4 w-full px-4">
-                      <Human />
-                      <span> Play vs Human </span>
-                    </div>
-                  </Link>
-                  <Link className="col-span-2 grid w-fit hover:bg-[#538538]" href='/'>
-                    <div className="flex flex-row gap-4 w-full px-4">
-                      <ChessBot />
-                      <span> Play vs Bot </span>
-                    </div>
-                  </Link>
-                </div>
-              </NavigationMenu.Content>
-            </NavigationMenu.Item>
-
-            <NavigationMenu.Item>
-              <NavigationMenu.Trigger asChild className="cursor-pointer hover:bg-[#538538]">
-                <div className="flex flex-row items-center justify-between px-4">
-                  <div className="flex flex-row gap-4">
-                    <Puzzle />
-                    <span> Puzzle </span>
-                  </div>
-                  <CaretRightIcon
-                    width={24}
-                    height={24}
-                    color="white"
-                    fontWeight={"bold"}
-                  />
-                </div>
-              </NavigationMenu.Trigger>
-              <NavigationMenu.Content>
-                <div className="one m-0 grid list-none gap-x-[8px] p-[18px] sm:w-[500px] sm:grid-cols-[0.75fr_1fr] gap-4">
-                  <Link className="col-span-2 grid w-fit hover:bg-[#538538]" href='/'>
-                    <div className="flex flex-row gap-4 w-full px-4">
-                      <Puzzle />
-                      <span> Play Puzzle </span>
-                    </div>
-                  </Link>
-                  <Link className="col-span-2 grid w-fit hover:bg-[#538538]" href='/'>
-                    <div className="flex flex-row gap-4 w-full px-4">
-                      <Duel />
-                      <span> Puzzle Duel </span>
-                    </div>
-                  </Link>
-                </div>
-              </NavigationMenu.Content>
-            </NavigationMenu.Item>
-
-            <NavigationMenu.Item>
-              <NavigationMenu.Trigger asChild className="cursor-pointer hover:bg-[#538538]">
-                <div className="flex flex-row items-center justify-between px-4">
-                  <div className="flex flex-row gap-4">
-                    <Learn />
-                    <span> Learn </span>
-                  </div>
-                  <CaretRightIcon
-                    width={24}
-                    height={24}
-                    color="white"
-                    fontWeight={"bold"}
-                  />
-                </div>
-              </NavigationMenu.Trigger>
-              <NavigationMenu.Content>
-                <div className="one m-0 grid list-none gap-x-[8px] p-[18px] sm:w-[500px] sm:grid-cols-[0.75fr_1fr] gap-4">
-                  <Link className="col-span-2 grid w-fit hover:bg-[#538538]" href='/'>
-                    <button className="flex flex-row gap-4 w-full px-4">
-                      <Video />
-                      <span> Videos </span>
-                    </button>
-                  </Link>
-                </div>
-              </NavigationMenu.Content>
-            </NavigationMenu.Item>
-            
-          </NavigationMenu.List>
-        </NavigationMenu.Root>
+      </Link>
+      <div>
+        <Menu
+          onClick={onClick}
+          mode="inline"
+          items={items}
+          className="bg-transparent text-white font-['Albula'] text-lg"
+        />
       </div>
     </div>
   );
