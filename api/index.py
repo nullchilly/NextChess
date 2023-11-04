7349fddb6b5f8edff0d1bd52d8f71e3de7a380b0
Null Chilly <nullchilly@gmail.com>
2023-11-04 21:37:16 +0700

    fix: fastapi api

--- api/index.py
+++ api/index.py
@@ -1,9 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse
 from .app import create_app
+from fastapi.staticfiles import StaticFiles
 
 app = create_app()
-
 
 @app.get("/api/healthchecker")
 def healthchecker():
     return {"status": "success", "message": "Integrate FastAPI Framework with Next.js"}
+
+app.mount("/", StaticFiles(directory="out", html=True), name="static")
