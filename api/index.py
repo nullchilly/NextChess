500b1d6a1756daecb49df3b65e5193ce5601d073
sownfam <ptson.0403@gmail.com>
2023-11-04 14:56:16 +0700

    feat: minimal fastapi setup

---
+++ api/index.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+
+app = FastAPI(docs_url="/api/docs", openapi_url="/api/openapi.json")
+
+
+@app.get("/api/healthchecker")
+def healthchecker():
+    return {"status": "success", "message": "Integrate FastAPI Framework with Next.js"}
