1141dd6c73755ebf5fa515968b92b3dcedaee162
Null Chilly <nullchilly@gmail.com>
2023-11-06 22:28:13 +0700

    fix(next): invalid boolean value

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  trailingSlash: 'true',
+  trailingSlash: true,
   output: 'export',
-  rewrites: async () => {
-    return [
-      {
-        source: "/api/:path*",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/api/:path*"
-            : "/api/",
-      },
-    ];
-  },
+  // rewrites: async () => {
+  //   return [
+  //     {
+  //       source: "/api/:path*",
+  //       destination:
+  //         process.env.NODE_ENV === "development"
+  //           ? "http://127.0.0.1:8000/api/:path*"
+  //           : "/api/",
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
