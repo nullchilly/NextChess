59475f7a7d5e31f7b3764031ad43fac9ba7944cc
sownfam <ptson.0403@gmail.com>
2023-11-13 19:33:58 +0700

    feat: almost full play vs bot

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  trailingSlash: true,
-  // output: 'export',
+  // trailingSlash: true,
   rewrites: async () => {
     return [
       {
