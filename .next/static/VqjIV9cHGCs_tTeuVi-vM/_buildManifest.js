5025cde7c79efb55d646fba2ddee256e4cc1c8de
Null Chilly <nullchilly@gmail.com>
2023-11-15 23:26:00 +0700

    feat: add release

---
+++ .next/static/VqjIV9cHGCs_tTeuVi-vM/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST=function(e){return{__rewrites:{afterFiles:[{has:e,source:"/api/:path*",destination:e}],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-d6885ef27f2c5e3d.js"],sortedPages:["/_app","/_error"]}}(void 0),self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ .next/static/chunks/app/playground/websocket/page-64f81daaef0a1153.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[484],{37703:function(e,n,r){Promise.resolve().then(r.bind(r,873))},873:function(e,n,r){"use strict";r.r(n),r.d(n,{default:function(){return PlaygroundSocket}});var t=r(57437),o=r(2265);function PlaygroundSocket(){let[e,n]=o.useState(),[r,s]=o.useState([]);return o.useEffect(()=>{let e=new WebSocket("".concat("ws://fall2324w3g9.int3306.freeddns.org")+"/ws");e.onopen=n=>{e.send("Connect")},e.onmessage=e=>{let n=JSON.parse(e.data);console.log("mess: ",n),s(e=>{var r;return[...e,null!==(r=n.message)&&void 0!==r?r:"EMPTY"]})},n(e)},[]),(0,t.jsxs)("div",{children:[(0,t.jsx)("p",{children:"Hello Socket!"}),(0,t.jsx)("button",{onClick:()=>{null==e||e.send("Hello how are you???")},children:"Send"}),r.map((e,n)=>(0,t.jsx)("div",{children:e},n))]})}},30622:function(e,n,r){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var t=r(2265),o=Symbol.for("react.element"),s=Symbol.for("react.fragment"),l=Object.prototype.hasOwnProperty,u=t.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,c={key:!0,ref:!0,__self:!0,__source:!0};function q(e,n,r){var t,s={},a=null,f=null;for(t in void 0!==r&&(a=""+r),void 0!==n.key&&(a=""+n.key),void 0!==n.ref&&(f=n.ref),n)l.call(n,t)&&!c.hasOwnProperty(t)&&(s[t]=n[t]);if(e&&e.defaultProps)for(t in n=e.defaultProps)void 0===s[t]&&(s[t]=n[t]);return{$$typeof:o,type:e,key:a,ref:f,props:s,_owner:u.current}}n.Fragment=s,n.jsx=q,n.jsxs=q},57437:function(e,n,r){"use strict";e.exports=r(30622)}},function(e){e.O(0,[971,472,744],function(){return e(e.s=37703)}),_N_E=e.O()}]);
