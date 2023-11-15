8b48a80e6292cde9ae3412dff626e0ea61d71348
Null Chilly <nullchilly@gmail.com>
2023-11-15 23:47:58 +0700

    feat: add release

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/:path*`
+        destination: `${process.env.BACKEND_URL}/api/:path*`
       },
     ];
   },
