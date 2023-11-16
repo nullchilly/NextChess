f4dfcb61746375636f9b0c04cf988444bdd700c7
Null Chilly <nullchilly@gmail.com>
2023-11-16 16:36:50 +0700

    feat: add release

---
+++ .next/static/mIW0dNCSqPO3cSu48HAJK/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST=function(e){return{__rewrites:{afterFiles:[{has:e,source:"/api/:path*",destination:e},{has:e,source:"/socket.io",destination:e}],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-1749fe2efd45c640.js"],sortedPages:["/_app","/_error"]}}(void 0),self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
