4869b1ffab0f57ac4cf96108f883fb69bbc76407
sownfam <ptson.0403@gmail.com>
2023-11-16 23:14:12 +0700

    fully integrated socketio with bot play

--- src/app/playground/socketio/page.tsx
+++ src/app/playground/socketio/page.tsx
@@ -1,44 +1,54 @@
-"use client"
-import React from "react"
-import { io } from "socket.io-client";
+"use client";
+import React from "react";
+import { io, Socket } from "socket.io-client";
 
 export default function PlaygroundSocket() {
-  const [websckt, setWebsckt] = React.useState<WebSocket>();
+  const [socket, setSocket] = React.useState<Socket | null>(null);
   const [messages, setMessages] = React.useState<string[]>([]);
 
-  // const socket = io("http://localhost:3000", { autoConnect: false, transports: ["websocket"] });
-  const socket = io(`${process.env.NEXT_PUBLIC_SOCKET_URL}`, { autoConnect: false, reconnection: false });
-
   React.useEffect(() => {
-
-    socket.connect()
-    socket.on("connect", () => { console.log("Connected", socket.id) }); 
-    // socket.emit('direct', 'Hello how are you???');
-    socket.on("response", (msg) => {
-      console.log("Response", msg)
-      // setMessages((prev) => [...prev, msg ?? "EMPTY"]);
-    });  
-
-  }, [])
+    if (socket) {
+      socket.connect();
+      socket.on("connect", () => {
+        console.log("Connected", socket.id);
+      });
+      // socket.emit('direct', 'Hello how are you???');
+      socket.on("response", (msg) => {
+        console.log("Response", msg);
+        // setMessages((prev) => [...prev, msg ?? "EMPTY"]);
+      });
+    }
+  }, [socket]);
 
   const handleOnClick = () => {
-    console.log("Send", "Hello how are you???")
-    socket.emit('direct', 'Hello how are you???');
+    console.log("Send", "Hello how are you???");
+    socket?.emit("direct", "Hello how are you???");
+  };
+
+  const handleConnect = () => {
+    const socket = io(`${process.env.NEXT_PUBLIC_SOCKET_URL}`, {
+      autoConnect: false,
+      reconnection: false,
+    });
+    setSocket(socket);
+  }
+
+  const handleDisconnect = () => {
+    socket?.close();
+    setSocket(null);
   }
 
   return (
     <div>
       <p>Hello Socket!</p>
-      <button onClick={handleOnClick}>
-        Send
-      </button>
+      <button onClick={handleOnClick}>Send</button>
+      <br/>
+      <button onClick={handleConnect}>Connect</button>
+      <br/>
+      <button onClick={handleDisconnect}>Disconnect</button>
       {messages.map((m, index) => {
-        return (
-            <div key={index}>
-              {m}
-            </div>
-        )
+        return <div key={index}>{m}</div>;
       })}
     </div>
-  )
+  );
 }
---
+++ src/hooks/useChessSocket.tsx
@@ -0,0 +1,127 @@
+import { useState } from 'react';
+import React from 'react'
+import { Chess, Move, ShortMove, Square } from 'chess.js';
+import { io, Socket } from "socket.io-client";
+
+export type ChessType = 'random' | 'computer' | 'minimax';
+
+enum ConnectionStatus {
+  Open = "open",
+  Connecting = "connecting",
+  Disconnected = "disconnected",
+}
+
+const useChessSocket = (type: ChessType) => {
+  // Start of socket
+  const [socket, setSocket] = React.useState<Socket | null>(null);
+  const [connectionStatus, setConnectionStatus] =
+    React.useState<ConnectionStatus>(ConnectionStatus.Disconnected);
+
+  const disconnectSocket = React.useCallback(() => {
+    socket?.close();
+    setSocket(null);
+    setConnectionStatus(ConnectionStatus.Disconnected);
+  }, [socket, setConnectionStatus]);
+
+  React.useEffect(() => {
+    if (socket) {
+      socket.connect();
+      socket.on("connect", () => {
+        console.log("Connected", socket.id);
+      });
+
+      socket.on("disconnect", () => {
+        disconnectSocket();
+      })
+
+      socket.on("play-chess", (msg) => {
+        console.log("Response", msg);
+        const message = JSON.parse(msg);
+        makeMove({
+          from: message['message']['from'],
+          to: message['message']['to'],
+          promotion: 'q',
+        });
+      });
+    }
+  }, [disconnectSocket, setConnectionStatus, socket]);
+
+  const sendMessage = (move: string) => {
+    console.log("MOVE: ", move);
+    socket?.emit("play-chess", move);
+  };
+
+  const connectSocket = () => {
+    const socket = io(`${process.env.NEXT_PUBLIC_SOCKET_URL}`, {
+      autoConnect: false,
+      reconnection: false,
+    });
+    setSocket(socket);
+  };
+
+  // End of socket
+  const [game, setGame] = useState(new Chess());
+  const [playing, setPlaying] = useState(false);
+  const [moves, setMoves] = useState<Move[]>([]);
+  const [depth, setDepth] = useState(3);
+  const [currentTimeout, setCurrentTimeout] = useState<NodeJS.Timeout>();
+
+  const makeMove = (move: string | ShortMove) => {
+    console.log(move)
+    const gameCopy = { ...game };
+    const result = gameCopy.move(move);
+
+    if (result) {
+      setMoves((prevMoves) => [result, ...prevMoves]);
+      setGame(gameCopy);
+    }
+
+    return result;
+  };
+
+  const onPieceDrop = (sourceSquare: Square, targetSquare: Square) => {
+    if (!playing) return false;
+
+    const move = makeMove({
+      from: sourceSquare,
+      to: targetSquare,
+      promotion: 'q',
+    });
+
+    if (move === null) return false;
+    sendMessage(move.from + move.to)
+    return true;
+  };
+
+  const resetGame = () => {
+    const gameCopy = { ...game };
+    gameCopy.reset();
+    setGame(gameCopy);
+
+    setMoves([]);
+
+    if (currentTimeout) clearTimeout(currentTimeout);
+    setPlaying(false);
+  };
+
+  const startGame = () => {
+    connectSocket();
+    resetGame();
+    setPlaying(true);
+  };
+
+  return {
+    game,
+    playing,
+    moves,
+
+    depth,
+    setDepth,
+
+    onPieceDrop,
+    startGame,
+    resetGame,
+  };
+};
+
+export default useChessSocket;
